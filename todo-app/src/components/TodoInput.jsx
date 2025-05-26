715c3d5dcc6004ede8c3dfebcca235977990ebb0
Thành Trần <ratosan19@gmail.com>
2025-05-26 18:21:32 +0700

    update done

--- todo-app/src/components/TodoInput.jsx
+++ todo-app/src/components/TodoInput.jsx
@@ -5,11 +5,25 @@ export function TodoInput(props) {
     // State để lưu trữ giá trị hiện tại của ô input
     const [inputValue, setInputValue] = useState('')
 
+    const handleSubmitTodo = () => {
+        if (!inputValue.trim()) { // Kiểm tra nếu inputValue rỗng hoặc chỉ chứa khoảng trắng
+            setInputValue(''); // Xóa input nếu nó chỉ là khoảng trắng
+            return;
+        }
+        handleAddTodo(inputValue); // Gọi hàm handleAddTodo (từ App.jsx) với giá trị input hiện tại
+        setInputValue(''); // Xóa nội dung ô input sau khi thêm
+    }
+
     return (
-        <div className="input-container"> {/* Container cho input và nút */}
+        <div className="input-container">
             <input
                 value={inputValue} // Giá trị của input được kiểm soát bởi state inputValue
                 onChange={(e) => { setInputValue(e.target.value) }} // Cập nhật inputValue mỗi khi người dùng gõ
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                        handleSubmitTodo(); // Gọi hàm handleSubmitTodo khi nhấn Enter
+                    }
+                }}
                 placeholder="Add a task" // Chữ gợi ý trong ô input
             />
             <button onClick={() => {
